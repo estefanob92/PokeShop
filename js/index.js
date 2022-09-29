c3fcf539c79328b801f920bfb9514a13c8868343
Estefano Bianchotti <estefanobianchotti@gmail.com>
2022-09-29 18:39:00 -0300

    fifth

--- js/index.js
+++ js/index.js
@@ -23,19 +23,9 @@ function createItem(item){
     contenedorItem.classList.add('contenedorItem');
     contenedorItem.classList.add('rounded');
 
-    /* const contenedorImagen = document.createElement('div');
-    contenedorImagen.classList.add('contenedorImagen');
-
-    const imagenItem = document.createElement('img');
-    imagenItem.src = item.sprites.front_default;
- 
-    contenedorImagen.appendChild(imagenItem)  */
-    
     const imagen = document.createElement('img');
     imagen.classList.add('imagenItem');
-
     const sprite = item.sprites.front_default;
-
     imagen.setAttribute('src', sprite)
     
 
@@ -46,7 +36,6 @@ function createItem(item){
     nombreItem.classList.add('nombreItem');
     nombreItem.textContent = item.name;
 
-    /* contenedorItem.appendChild(contenedorImagen); */
     contenedorItem.appendChild(numeroItem);
     contenedorItem.appendChild(imagen);
     contenedorItem.appendChild(nombreItem);
